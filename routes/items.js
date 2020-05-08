f779e2d96cd5243c2397ce53b53add2b26d032ba
Luis Botelho <luisbotelho1996@gmail.com>
2020-05-08 02:16:19 +0000

    add item create route

--- routes/items.js
+++ routes/items.js
@@ -4,6 +4,8 @@ var router = express.Router({mergeParams:true});
 var Category = require("../models/category");
 var Item = require("../models/item");
 
+
+//New Route
 router.get("/new",function(req,res){
 	Category.findById(req.params.id,function(err,category){
         console.log(req.params.id)
@@ -15,6 +17,40 @@ router.get("/new",function(req,res){
 			res.render("items/new",{category:category});
 		}
 	})   
+});
+
+//Create Route
+router.post("/",function(req,res){
+    console.log(req.body)
+    Category.findById(req.params.id,function(err,category){
+        if(err){
+            console.log(err);
+            res.redirect("/categories");
+        }
+        else{
+            // console.log(category);
+            var name = req.body.name;
+            var description = req.body.description;
+            var image = req.body.image;
+            var eloScore= 1400;
+            var newItem = {name:name,description:description,image:image,eloScore:eloScore};
+            // console.log(newItem);
+            Item.create(newItem,function(err,item){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    //Add username and id to comment
+                    //save comment
+                   
+                    category.items.push(item);
+                    category.save();
+                    var url = "/categories/" + category._id;
+                    res.redirect(url);
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
